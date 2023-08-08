f66c9986dd8bc024ef05919314bd7a8b4dcc021e
Pawel Sawicki <public@psawicki.de>
2023-08-08 15:36:12 +0200

    normalize.js

---
+++ normalize.js
@@ -0,0 +1,24 @@
+const persons = [
+    {
+        id: '1',
+        first: 'Max',
+        last: 'Mustermann',
+    },
+    {
+        id: '2',
+        first: 'Mina',
+        last: 'Musterfrau',
+    }
+];
+
+
+const personsNormalized = {
+    '1': {
+        first: 'Max',
+        last: 'Mustermann',
+    },
+    '2': {
+        first: 'Mina',
+        last: 'Musterfrau',
+    }
+};
